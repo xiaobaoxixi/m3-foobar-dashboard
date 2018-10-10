332a630a141e13371c07897cb9784fe532f5bd85
liyan <yanx0167@stud.kea.dk>
2018-10-10 06:59:04 +0200

    styling to average order section

--- statistic.js
+++ statistic.js
@@ -1,8 +1,12 @@
 "use strict";
 
+const average = document.querySelector(".average-order p:nth-of-type(2)");
+
 function popularBeer(totalCustomerCount, totalOrder) {
   console.log(totalCustomerCount, totalOrder);
   let totalBeerCount = totalOrder.length;
   let averageOrdersize = totalBeerCount / totalCustomerCount;
+  let averageOrdersizeCLeared = Math.floor(averageOrdersize * 1000) / 1000;
   console.log(averageOrdersize);
+  average.innerHTML = `<span class="bold">${averageOrdersizeCLeared}</span> glass/customer`;
 }
